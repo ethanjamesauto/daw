18a44fc090b27c62fde0984be1bc8df1efc18dd6
Ethan James <ethanjamesauto@vt.edu>
2023-05-10 16:25:00 -0400

    update vitis files

--- vitis/system_wrapper/platform.tcl
+++ vitis/system_wrapper/platform.tcl
@@ -2,3 +2,6 @@ platform generate -domains
 platform generate
 platform generate
 platform generate
+platform active {system_wrapper}
+platform config -updatehw {C:/Users/Ethan/Documents/daw/vivado/hw/system_wrapper.xsa}
+platform clean
